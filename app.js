b1b717b9572a742b12808eb88e8a2178347f832b
ArdyBeheshti <ardybeheshti@gmail.com>
2017-09-03 07:31:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+var express = require('express')
+var app = express()
 
-const app = express();
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
-/*
-const logger = function(req, res, next){
-	console.log('logging...');
-	next();
-}
-
-app.use(logger);
-*/
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-// Set Static Path
-app.use(express.static(path.join(__dirname, 'public')));
-
-/*
-var person = {
-	name: 'Jeff',
-	age: 30
-}
-*/
-
-app.get('/', function(req, res, next){
-	res.render('index.html');
-	res.render('main.html')
-});
-
-app.listen(3000, function(){
-	console.log('Server started on port 3000...');
+app.get('/', function(request, response) {
+  response.send('Hello World!')
 })
+
